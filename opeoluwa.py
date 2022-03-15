8afda18c614cd3e34fd6b7a969adbbc2bc95b8cb
op-techy <101581466+op-techy@users.noreply.github.com>
2022-03-15 11:24:04 +0100

    Create opeoluwa.py

---
+++ opeoluwa.py
@@ -0,0 +1,11 @@
+# Task 1
+
+myFullName = 'Babalola Opeoluwa'
+phoneNum = '07052078722'
+Myemail = 'babalolaope45@gmail.com'
+Mylang = 'python/html/javascript'
+MyPath = ' Designer /FrontEnd Developer'
+
+myIntro = 'Hello world, My name is '+myFullName+' with contact '+phoneNum+' and '\
+          +Myemail+' using '+Mylang+' as the language for my first journey as a '+MyPath
+print(myIntro)
